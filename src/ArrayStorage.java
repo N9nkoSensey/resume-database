3322ab69177de1af7129ca8edf84ac4c8a351044
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-21 23:10:39 +0300

    Refactoring ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,37 +1,38 @@
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
-    void save(Resume resume) {
-
-        boolean isClear = true;
-        if (resume != null && size() < storage.length) {
-            for (int i = 0; i < size(); i++) {
+    public boolean update(Resume resume) {
+        if (resume != null && size < storage.length) {
+            for (int i = 0; i < size; i++) {
                 if (resume.uuid.equals(storage[i].uuid)) {
-                    isClear = false;
                     System.out.println("Duplicate resume");
-                    break;
+                    return false;
                 }
-            }
-            if (isClear) {
-                storage[size()] = resume;
             }
         } else {
             System.out.println("Can't execute method \"save\". Either an empty summary or an array filled");
         }
+        return true;
     }
 
-    Resume get(String uuid) {
+    public void save(Resume resume) {
+        if (update(resume)) {
+            storage[size] = resume;
+            size++;
+        }
+    }
 
+    public Resume get(String uuid) {
         if (uuid != null || !uuid.isEmpty()) {
-            for (int i = 0; i < size(); i++) {
+            for (int i = 0; i < size; i++) {
                 if (uuid.equals(storage[i].uuid)) {
                     return storage[i];
                 }
@@ -40,13 +41,12 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
-
-        int size = size();
+    public void delete(String uuid) {
         if (uuid != null || !uuid.isEmpty()) {
             for (int i = 0; i < size; i++) {
                 if (storage[i] != null && storage[i].uuid.equals(uuid)) {
                     storage[i] = null;
+                    size--;
                 }
 
                 if (storage[i] == null && storage[i + 1] != null && i != (storage.length - 1)) {
@@ -57,27 +57,13 @@ public class ArrayStorage {
         }
     }
 
-    Resume[] getAll() {
-
-        Resume[] resumesWithoutNull = new Resume[size()];
-        if (size() >= 0) System.arraycopy(storage, 0, resumesWithoutNull, 0, size());
+    public Resume[] getAll() {
+        Resume[] resumesWithoutNull = new Resume[size];
+        if (size > 0) System.arraycopy(storage, 0, resumesWithoutNull, 0, size);
         return resumesWithoutNull;
     }
 
-    int size() {
-
-        if (storage[storage.length - 1] != null) {
-            return storage.length;
-        }
-
-        int storageSize = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storageSize = i;
-                break;
-            }
-        }
-
-        return storageSize;
+    public int size() {
+        return size;
     }
 }
