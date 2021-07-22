1ec21501d2980cdee2158d6f62d90663fe2a1d4f
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-22 16:36:26 +0300

    Introduce interface

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -2,37 +2,38 @@ package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.storage.Storage;
 
 public class MainArray {
-    static ArrayStorage arrayStorage = new ArrayStorage();
+  private static final Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        final Resume r1 = new Resume();
         r1.setUuid("uuid1");
-        Resume r2 = new Resume();
+        final Resume r2 = new Resume();
         r2.setUuid("uuid2");
-        Resume r3 = new Resume();
+        final Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
-        arrayStorage.save(r1);
-        arrayStorage.save(r2);
-        arrayStorage.save(r3);
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
 
-        System.out.println("Get r1: " + arrayStorage.get(r1.getUuid()));
-        System.out.println("Size: " + arrayStorage.size());
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
 
         printAll();
-        arrayStorage.delete(r1.getUuid());
+        ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
-        arrayStorage.clear();
+        ARRAY_STORAGE.clear();
         printAll();
 
-        System.out.println("Size: " + arrayStorage.size());
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
         System.out.println("\nGet All");
-        for (Resume r : arrayStorage.getAll()) {
+        for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
         }
     }
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,69 +2,70 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.nio.file.Path;
 import java.util.Arrays;
 
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+public class ArrayStorage implements Storage{
+    private static final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
+    @Override
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
+    @Override
     public void update(Resume resume) {
         int index = getIndex(resume.getUuid());
         if (index == -1) {
             System.out.println("Resume " + resume.getUuid() + " not exist.");
         } else {
             storage[index] = resume;
-            size++;
         }
     }
 
+    @Override
     public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) != -1){
+        if (getIndex(resume.getUuid()) != -1) {
             System.out.println("Resume " + resume.getUuid() + " already exist.");
-        }else if (size == storage.length){
+        } else if (size == STORAGE_LIMIT) {
             System.out.println("Storage overflow");
-        } else{
+        } else {
             storage[size] = resume;
             size++;
         }
     }
 
+    @Override
     public Resume get(String uuid) {
-        if (uuid != null || !uuid.isEmpty()) {
-            for (int i = 0; i < size; i++) {
-                if (uuid.equals(storage[i].getUuid())) {
-                    return storage[i];
-                }
-            }
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist.");
+            return null;
         }
-        return null;
+        return storage[index];
     }
 
+    @Override
     public void delete(String uuid) {
-        if (uuid != null || !uuid.isEmpty()) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i] != null && storage[i].getUuid().equals(uuid)) {
-                    storage[i] = null;
-                    size--;
-                }
-
-                if (storage[i] == null && storage[i + 1] != null && i != (storage.length - 1)) {
-                    storage[i] = storage[i + 1];
-                    storage[i + 1] = null;
-                }
-            }
+        int index = getIndex(uuid);
+        if (index == -1){
+            System.out.println("Resume " + uuid + " not exist.");
+        }else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
     }
 
+    @Override
     public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
+    @Override
     public int size() {
         return size;
     }
