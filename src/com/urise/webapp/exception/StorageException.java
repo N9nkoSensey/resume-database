4a171770b1a5cb311f0ca1c2d87ad78e7d8b5cc3
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-27 12:30:50 +0300

    Added ObjectStreamPathStorage

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -1,12 +1,10 @@
 package com.urise.webapp.exception;
 
-import java.io.IOException;
-
 public class StorageException extends RuntimeException {
     private final String uuid;
 
-    public StorageException(String uuid) {
-        this.uuid = uuid;
+    public StorageException(String message) {
+        this(message, null, null);
     }
 
     public StorageException(String message, String uuid) {
