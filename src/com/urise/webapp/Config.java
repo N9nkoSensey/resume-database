abeb570d13beb31a3b7e35924041e7ea5fc62765
Igor Strelchenya <77210968+N9nkoSensey@users.noreply.github.com>
2021-07-31 15:50:48 +0300

    Deploy database Heroku and war file

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -7,7 +7,8 @@ import java.io.*;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
+    private static final String PROPS = "/resumes.properties";
+
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -18,14 +19,14 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream inputStream = new FileInputStream(PROPS)) {
+        try (InputStream inputStream = Config.class.getResourceAsStream(PROPS)) {
             Properties properties = new Properties();
             properties.load(inputStream);
             storageDir = new File(properties.getProperty("storage.dir"));
             storage = new SqlStorage(properties.getProperty("db.url"),
                     properties.getProperty("db.user"), properties.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
@@ -35,16 +36,6 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
-    }
-
-    private static File getHomeDir(){
-        String property = System.getProperty("homeDir");
-
-        File homeDir = new File(property == null ? "." : property);
-        if (!homeDir.isDirectory()){
-          throw new IllegalStateException(homeDir + " is not directory");
-        }
-        return homeDir;
     }
 }
 
